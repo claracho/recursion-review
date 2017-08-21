d18a1d693f490f80b5a443c5c92de40796f3c3ae
Hack Reactor Student <student@hackreactor.com>
2017-08-21 12:32:58 -0700

    completed stringifyJSON and getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,21 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
+  // use document.body, element.childNodes, and element.classList
+  var node = arguments[1] || document.body;
+  var result = arguments[2] || [];
+
+  // if the node.classList contains className, then push node
+  if (node.classList && node.classList.contains(className)) {
+    result.push(node);
+  }
+  // if node.childNodes exist, run getElementsByClassName(childNodes) for each child
+  if (node.hasChildNodes()) {
+    var children = node.childNodes;
+    for (var i = 0; i < children.length; i++) {
+      getElementsByClassName(className, children[i], result);
+    }
+  }
+
+  return result;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,24 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  if (Array.isArray(obj)) {
+    var arr = [];
+    for (var i = 0; i < obj.length; i++) {
+      arr.push(stringifyJSON(obj[i]));
+    }
+    return '[' + arr + ']';
+  }
+  if (typeof obj === 'object' && obj !== null) {
+    var objArr = [];
+    for (var key in obj) {
+      if (typeof obj[key] !== 'function' && typeof obj[key] !== 'undefined') {
+        objArr.push('"' + key + '":' + stringifyJSON(obj[key]));
+      }
+    }
+    return '{' + objArr + '}';
+  }
+  return '' + obj;
 };
